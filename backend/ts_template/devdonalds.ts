23fc8bb86c6715d93b8321c7ab6434e1a59ba1da
William Gaston <z5591798@ad.unsw.edu.au>
2025-02-26 22:53:08 +1100

    finished tasks

--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json());
 
 // Store your recipes here!
-const cookbook: any = null;
+const cookbook: (recipe | ingredient)[] = [];
 
 // Task 1 helper (don't touch)
 app.post("/parse", (req:Request, res:Response) => {
@@ -45,23 +45,136 @@ app.post("/parse", (req:Request, res:Response) => {
 // [TASK 1] ====================================================================
 // Takes in a recipeName and returns it in a form that 
 const parse_handwriting = (recipeName: string): string | null => {
-  // TODO: implement me
+  recipeName = recipeName.replace('-', ' ');
+  recipeName = recipeName.replace('_', ' ');
+
+  recipeName = recipeName.replace(/[^A-Za-z\s]/g, '');
+
+  recipeName = recipeName.replace(/\s{2,}/g, ' ').trim();
+  recipeName = recipeName.toLowerCase();
+  recipeName = recipeName.replace(/\b\w/g, letter => letter.toUpperCase());
+
+  if (recipeName.length <= 0) return null;
+
   return recipeName
 }
 
 // [TASK 2] ====================================================================
 // Endpoint that adds a CookbookEntry to your magical cookbook
 app.post("/entry", (req:Request, res:Response) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
+  const {type, name, requiredItems, cookTime} = req.body;
+
+  if (type === null || name === null || (type === 'recipe' && requiredItems === null) || (type === 'ingredient' && cookTime === null)) {
+    return res.status(400).json({ error: "invalid input received" });
+  }
+
+  const names = cookbook.map(element => element.name.toLowerCase());
+  const nameSet = new Set(names);
+  nameSet.add(name.toLowerCase());
+  if ((names.length + 1) !== nameSet.size) {
+      return res.status(400).json({ error: "name already in cookbook" });
+  }
+
+  switch (type) {
+    case "recipe":
+
+      const newRecipe: recipe = {
+        type,
+        name,
+        requiredItems: requiredItems as requiredItem[],
+      }
+
+      if (!Array.isArray(requiredItems)) {
+        return res.status(400).json({ error: "invalid items type" });
+      } else {
+        const ingredientNames = requiredItems.map(element => element.name.toLocaleLowerCase());
+        const ingredientNamesSet = new Set(ingredientNames);
+
+        for (const ingredient of requiredItems) {
+          ingredientNamesSet.add(ingredient);
+        }
+
+        if ((ingredientNames.length + requiredItems.length) !== ingredientNamesSet.size) {
+          return res.status(400).json({ error: "ingredient names are not unique" });
+        } else if (requiredItems.length === 0) {
+          return res.status(400).json({ error: "no ingredients given" });
+        } else {
+          cookbook.push(newRecipe);
+          return res.status(200).json({});
+        }
+      }
+    case "ingredient":
+
+      const newIngredient: ingredient = {
+        type,
+        name,
+        cookTime,
+      }
+    
+      if (typeof cookTime !== "number" || cookTime < 0) {
+        return res.status(400).json({ error: "invalid cook time" });
+      } else {
+        cookbook.push(newIngredient);
+        return res.status(200).json({});
+      }
+    default:
+      res.status(400).send("invalid type");
+  }
 
 });
 
 // [TASK 3] ====================================================================
 // Endpoint that returns a summary of a recipe that corresponds to a query name
 app.get("/summary", (req:Request, res:Request) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
+  const name: string = req.query.name;
+
+  if (cookbook.length === 0 || cookbook === undefined) {
+    res.status(400).send("cookbook has no entries");
+  }
+
+  let overview: recipe | ingredient = cookbook.find((element) => element.name == name);
+
+  if (overview.type === 'ingredient' || overview === undefined) {
+    res.status(400).send("invalid name given");
+  }
+
+  const outputArray: recipe[] & ingredient[] = [];
+  const objectQueue = [name];
+  const usedItems = new Set();
+
+  while (objectQueue.length > 0) {
+    const currElement = objectQueue.pop();
+    let overview: recipe | ingredient = cookbook.find((element) => element.name == currElement);
+    if (overview === undefined) {
+      res.status(400).send("item not in cookbook");
+    }
+    switch (overview.type) {
+      case "ingredient":
+        overview = overview as ingredient;
+        if (!usedItems.has(overview.name)) {
+          outputArray.push(overview);
+        }
+        break;
+
+      case "recipe":
+        overview = overview as recipe;
+        if (!usedItems.has(overview.name)) {
+          outputArray.push(overview);
+        }
+
+        for (const element of overview.requiredItems) {
+          if (!usedItems.has(element.name)) {
+            objectQueue.push(element.name);
+          }
+        }
+        break;
+    }
+
+    usedItems.add(overview.name);
+
+  }
+
+  res.status(200).json({outputArray})
 
 });
 
